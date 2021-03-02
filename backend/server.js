d46cd6163f79edfe132c59c9bdb1a5823976f1ea
Aranel <aranel@example.com>
2021-03-02 19:47:36 +0800

    Created models for MongoDB and create connections to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import colors from 'colors'
 import products from './data/products.js'
 
 dotenv.config()
+
+connectDB()
 
 const app = express()
 
@@ -23,5 +27,6 @@ app.listen(
   process.env.PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`
+      .yellow.bold
   )
 )
